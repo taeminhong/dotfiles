c8f25c387ae4902f9919ee0ec37ba2fcc3f41e8b
Taemin Hong <taeminhong@outlook.com>
2017-12-23 15:30:14 +0900

    fzf: change keybindings if editing-mode is emacs

--- .fzf-keybinding-patch.bash
+++ .fzf-keybinding-patch.bash
@@ -1,15 +1,17 @@
-# roll back keybindings
-bind '"\C-t": transpose-chars'
-bind '"\ec": capitalize-word'
+if [ -o emacs ]; then
+    # roll back keybindings
+    bind '"\C-t": transpose-chars'
+    bind '"\ec": capitalize-word'
 
-# Alt-j cd into the selected directory
-bind '"\ej": " \C-e\C-u`__fzf_cd__`\e\C-e\er\C-m"'
+    # Alt-j cd into the selected directory
+    bind '"\ej": " \C-e\C-u`__fzf_cd__`\e\C-e\er\C-m"'
 
-# Alt-i - Paste the selected file path into the command line
-if [ $BASH_VERSINFO -gt 3 ]; then
-    bind -x '"\ei": "fzf-file-widget"'
-elif __fzf_use_tmux__; then
-    bind '"\ei": " \C-u \C-a\C-k`__fzf_select_tmux__`\e\C-e\C-y\C-a\C-d\C-y\ey\C-h"'
-else
-    bind '"\ei": " \C-u \C-a\C-k`__fzf_select__`\e\C-e\C-y\C-a\C-y\ey\C-h\C-e\er \C-h"'
+    # Alt-i - Paste the selected file path into the command line
+    if [ $BASH_VERSINFO -gt 3 ]; then
+        bind -x '"\ei": "fzf-file-widget"'
+    elif __fzf_use_tmux__; then
+        bind '"\ei": " \C-u \C-a\C-k`__fzf_select_tmux__`\e\C-e\C-y\C-a\C-d\C-y\ey\C-h"'
+    else
+        bind '"\ei": " \C-u \C-a\C-k`__fzf_select__`\e\C-e\C-y\C-a\C-y\ey\C-h\C-e\er \C-h"'
+    fi
 fi
