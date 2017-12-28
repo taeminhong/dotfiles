6850c88ae3de3b007c60e6157bed62e4c205fb7c
Taemin Hong <taeminhong@outlook.com>
2017-12-28 14:45:36 +0900

    Re-indent bootstrapsh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,56 +3,56 @@
 cd "$(dirname "${BASH_SOURCE}")";
 
 function append_line() {
-	set -e
+    set -e
 
-	local line="$1"
-	local file="$2"
-	local pattern="$3"
-	local lineno=""
+    local line="$1"
+    local file="$2"
+    local pattern="$3"
+    local lineno=""
 
-	if [ -f "$file" ]; then
-		lineno=$(grep -nF "$pattern" "$file" | sed 's/:.*//' | tr '\n' ' ')
-		if [ -z "$lineno" ]; then
-		    echo "$line" >> "$file"
-		fi
-	fi
+    if [ -f "$file" ]; then
+        lineno=$(grep -nF "$pattern" "$file" | sed 's/:.*//' | tr '\n' ' ')
+        if [ -z "$lineno" ]; then
+            echo "$line" >> "$file"
+        fi
+    fi
 
-	set +e
+    set +e
 }
 
 function platform() {
-	local name=$(expr $(uname) : '^\([a-zA-Z]*\)')
-	case $name in
-		CYGWIN|MINGW|MSYS) echo "Windows";;
-		Darwin) echo "Mac";;
-		*) echo $name;;
-	esac
+    local name=$(expr $(uname) : '^\([a-zA-Z]*\)')
+    case $name in
+        CYGWIN|MINGW|MSYS) echo "Windows";;
+        Darwin) echo "Mac";;
+        *) echo $name;;
+    esac
 }
 
 function doIt() {
-	local fzf_patch=".fzf-keybinding-patch.bash"
-	rsync --exclude ".git/" \
-		--exclude ".DS_Store" \
-		--exclude ".osx" \
-		--exclude "bootstrap.sh" \
-		--exclude "README.md" \
-		--exclude "fzf*" \
-		--exclude "keyboard" \
-		--exclude "Mac" \
-		--exclude "Linux" \
-		--exclude "Windows" \
-		-avh --no-perms . "$(platform)/" ~;
-	append_line "[ -f ~/$fzf_patch ] && source ~/$fzf_patch" ~/.fzf.bash "$fzf_patch"
-	source ~/.bash_profile;
+    local fzf_patch=".fzf-keybinding-patch.bash"
+    rsync --exclude ".git/" \
+          --exclude ".DS_Store" \
+          --exclude ".osx" \
+          --exclude "bootstrap.sh" \
+          --exclude "README.md" \
+          --exclude "fzf*" \
+          --exclude "keyboard" \
+          --exclude "Mac" \
+          --exclude "Linux" \
+          --exclude "Windows" \
+          -avh --no-perms . "$(platform)/" ~;
+    append_line "[ -f ~/$fzf_patch ] && source ~/$fzf_patch" ~/.fzf.bash "$fzf_patch"
+    source ~/.bash_profile;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
+    doIt;
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
+    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+    echo "";
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        doIt;
+    fi;
 fi;
 unset doIt;
