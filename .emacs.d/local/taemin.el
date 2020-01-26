fd71ae039b803c2c9642e3061322713e9442fdd8
Taemin Hong <taeminhong@outlook.com>
2020-01-26 12:50:41 +0900

    taemin.el: follow the elisp naming convention
    The function prefix 'my-' has changed to 'taemin-' for match with the file name.
    Internal functions now have '--' in their names.

--- .emacs.d/local/taemin.el
+++ .emacs.d/local/taemin.el
@@ -1,6 +1,6 @@
 (require 'misc)
 
-(defun my-forward-to-word (n)
+(defun taemin-forward-to-word (n)
   (interactive "^p")
   (let (word-begin
         word-end
@@ -18,7 +18,7 @@
         (goto-char word-end))
       (setq n (1- n)))))
 
-(defun my-kill-word (n)
+(defun taemin-kill-word (n)
   (interactive "^p")
   (while (< 0 n)
     (if (looking-at-p "\\w")
@@ -28,7 +28,7 @@
         (kill-region beg end)))
     (setq n (1- n))))
 
-(defun my-backward-kill-word (n)
+(defun taemin-backward-kill-word (n)
   (interactive "^p")
   (while (< 0 n)
     (if (looking-back "\\w" (- (point) 1))
@@ -38,13 +38,13 @@
         (kill-region beg end)))
     (setq n (1- n))))
 
-(defun last-sexp-in-list-p ()
+(defun taemin--last-sexp-in-list-p ()
   (condition-case nil
       (save-excursion
         (forward-sexp) nil)
     (scan-error nil t)))
 
-(defun no-more-sexp-p ()
+(defun taemin--no-more-sexp-p ()
   (let ((prev-point (point)))
     (condition-case err
       (save-excursion
@@ -54,75 +54,75 @@
         (<= (point) prev-point))
       (scan-error nil t))))
 
-(defun my-next-sexp-iter (start-point n)
+(defun taemin-next-sexp-iter (start-point n)
   (cond ((<= n 0) (point))
-        ((last-sexp-in-list-p)
+        ((taemin--last-sexp-in-list-p)
          (when (= (point) start-point)
            ;; raise a scan error
            (forward-sexp)))
-        ((no-more-sexp-p)
+        ((taemin--no-more-sexp-p)
          (forward-sexp))
         (t
          (forward-sexp)
          (backward-sexp)
          (if (> (point) start-point)
-             (my-next-sexp-iter (point) (- n 1))
+             (taemin-next-sexp-iter (point) (- n 1))
            (progn (forward-sexp)
-                  (my-next-sexp-iter start-point n))))))
+                  (taemin-next-sexp-iter start-point n))))))
 
-(defun my-next-sexp (n)
+(defun taemin-next-sexp (n)
   (interactive "^p")
   (cond ((< n 0) (backward-sexp (- n)))
-        (t (my-next-sexp-iter (point) n))))
+        (t (taemin-next-sexp-iter (point) n))))
 
-(defun make-range (beg end)
+(defun taemin--make-range (beg end)
   (cons beg end))
 
-(defun beginning (range)
+(defun taemin--range-beginning (range)
   (car range))
 
-(defun end (range)
+(defun taemin--range-end (range)
   (cdr range))
 
-(defun nearby-defun (pos)
+(defun taemin--nearby-defun (pos)
   (let (end)
     (save-excursion
       (goto-char pos)
       (end-of-defun)
       (setq end (point))
       (beginning-of-defun)
-      (make-range (point) end))))
+      (taemin--make-range (point) end))))
 
-(defun my-next-defun (n)
+(defun taemin-next-defun (n)
   (interactive "^p")
   (cond ((< n 0)
          (beginning-of-defun (- n)))
         ((> n 0)
-         (or (not (eq this-command 'my-next-defun))
-             (eq last-command 'my-next-defun)
+         (or (not (eq this-command 'taemin-next-defun))
+             (eq last-command 'taemin-next-defun)
              (and transient-mark-mode mark-active)
              (push-mark))
-         (my-next-defun-iter (point) n))))
+         (taemin-next-defun-iter (point) n))))
 
-(defun my-next-defun-iter (start-point n)
+(defun taemin-next-defun-iter (start-point n)
   (cl-assert (<= start-point (point)))
   (cond ((<= n 0)
          (point))
         ((= start-point (point-max))
          (signal 'end-of-buffer start-point))
         (t
-         (let ((nearby (nearby-defun (point))))
-           (cond ((> (beginning nearby) start-point)
-                  (goto-char (beginning nearby))
-                  (my-next-defun-iter (beginning nearby) (- n 1)))
-                 ((> (end nearby) (point))
-                  (goto-char (end nearby))
-                  (my-next-defun-iter start-point n))
+         (let ((nearby (taemin--nearby-defun (point))))
+           (cond ((> (taemin--range-beginning nearby) start-point)
+                  (goto-char (taemin--range-beginning nearby))
+                  (taemin-next-defun-iter (taemin--range-beginning nearby) (- n 1)))
+                 ((> (taemin--range-end nearby) (point))
+                  (goto-char (taemin--range-end nearby))
+                  (taemin-next-defun-iter start-point n))
                  (t
                   (goto-char (point-max))
-                  (my-next-defun-iter (point-max) (- n 1))))))))
+                  (taemin-next-defun-iter (point-max) (- n 1))))))))
 
-(defun my-electric-pair-conservative-inhibit (char)
+(defun taemin-electric-pair-conservative-inhibit (char)
   "Customized version of `electric-pair-pair-conservative-inhibit'"
   (or
    ;; I find it more often preferable not to pair when the
@@ -139,7 +139,7 @@
         (> (point) 2)
         (eq (char-syntax (char-before (1- (point)))) ?w))))
 
-(defun end-of-defun-spaces (n)
+(defun taemin--end-of-defun-spaces (n)
   (cl-assert (> n 0))
   (progn (dotimes (_ n)
            (let ((old-point (point)))
@@ -150,9 +150,9 @@
            (skip-chars-forward "[:space:]\n")
            (skip-chars-backward "[:space:]"))))
 
-(defun expand-defun-mark (arg beg end)
+(defun taemin--expand-defun-mark (arg beg end)
   (cond ((> arg 0)
-         (end-of-defun-spaces arg)
+         (taemin--end-of-defun-spaces arg)
          (setq end (point))
          (push-mark beg nil t)
          (goto-char end))
@@ -165,13 +165,13 @@
            (beginning-of-defun (1- (- arg))))
          (push-mark end nil t))))
 
-(defun turning-back-mark-defun-p ()
+(defun taemin--turning-back-mark-defun-p ()
   (and (region-active-p)
        (= (point) (mark))
        (eq this-command last-command)
-       (member last-command '(my-mark-defun-back my-mark-defun))))
+       (member last-command '(taemin-mark-defun-back taemin-mark-defun))))
 
-(defun my-mark-defun (&optional arg)
+(defun taemin-mark-defun (&optional arg)
   "Put mark at beginning of this defun, point at beginning of next defun.
 The defun marked is the one that contains point or follows point.
 With positive ARG, mark this and that many next defuns; with negative
@@ -181,16 +181,16 @@ If the mark is active, it marks the next or previous defun(s) after
 the one(s) already marked."
   (interactive "p")
   (setq arg (or arg 1))
-  ;; There is no `my-mark-defun-back' function - see
+  ;; There is no `taemin-mark-defun-back' function - see
   ;; https://lists.gnu.org/r/bug-gnu-emacs/2016-11/msg00079.html
   ;; for explanation
-  (when (eq last-command 'my-mark-defun-back)
+  (when (eq last-command 'taemin-mark-defun-back)
     (setq arg (- arg)))
   (when (< arg 0)
-    (setq this-command 'my-mark-defun-back))
-  (cond ((or (use-region-p) (turning-back-mark-defun-p))
+    (setq this-command 'taemin-mark-defun-back))
+  (cond ((or (use-region-p) (taemin--turning-back-mark-defun-p))
          (if (>= arg 0)
-             (end-of-defun-spaces arg)
+             (taemin--end-of-defun-spaces arg)
            (beginning-of-defun-comments (- arg))))
         (t
          (let ((opoint (point))
@@ -201,7 +201,7 @@ the one(s) already marked."
            ;; offside rule, e.g. Python.
            (beginning-of-defun-comments)
            (setq beg (point))
-           (end-of-defun-spaces 1)
+           (taemin--end-of-defun-spaces 1)
            (setq end (point))
            (when (or (and (<= (point) opoint)
                           (> arg 0))
@@ -209,11 +209,11 @@ the one(s) already marked."
              ;; beginning-of-defun moved back one defun so we got the wrong
              ;; one.  If ARG < 0, however, we actually want to go back.
              (goto-char opoint)
-             (end-of-defun-spaces 1)
+             (taemin--end-of-defun-spaces 1)
              (setq end (point))
              (beginning-of-defun-comments)
              (setq beg (point)))
            (goto-char beg)
-           (expand-defun-mark arg beg end)))))
+           (taemin--expand-defun-mark arg beg end)))))
 
 (provide 'taemin)
