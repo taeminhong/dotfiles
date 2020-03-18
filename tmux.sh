6f4f0152c9661bc3955bce7cdd6d57a508c24660
Taemin Hong <taeminhong@outlook.com>
2020-03-18 16:58:39 +0900

    tmux.sh: attach to the most recently used session instead of "home"

--- tmux.sh
+++ tmux.sh
@@ -1,10 +1,11 @@
-# When you invoke tmux without any arguments it will try to attach to the default session.
-# If there's no such session, it will make a new one with that name.
+# When you invoke tmux without any arguments it will try to attach to the most
+# recently used session.
+# If there's no session, it will make a new one with the default session name.
 
 _tmux () {
     if test $# -eq 0
     then
-        \tmux a -t ${TMUX_DEFAULT_SESSION:-home} || \tmux new -s ${TMUX_DEFAULT_SESSION:-home}
+        \tmux a || \tmux new -s ${TMUX_DEFAULT_SESSION:-home}
     else
         \tmux "$@"
     fi
