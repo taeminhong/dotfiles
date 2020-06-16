4603382a08caa03d9e0cf0e8acc41adb03a0adf6
Taemin Hong <taeminhong@outlook.com>
2020-06-16 20:16:06 +0900

    .fzf_keybinding_patch.bash: remove useless condition
    I don't use the vi mode so it is not needed to check if it's in the emacs mode.

--- .fzf-keybinding-patch.bash
+++ .fzf-keybinding-patch.bash
@@ -1,21 +1,19 @@
-if [ -o emacs ]; then
-    # roll back keybindings
-    bind '"\C-t": transpose-chars'
-    bind '"\ec": capitalize-word'
-    bind '"\C-r": reverse-search-history'
+# roll back keybindings
+bind '"\C-t": transpose-chars'
+bind '"\ec": capitalize-word'
+bind '"\C-r": reverse-search-history'
 
-    # Alt-p Paste the selected command from history into the command line
-    bind '"\ep": " \C-e\C-u`__fzf_history__`\e\C-e\e^\er"'
+# Alt-p Paste the selected command from history into the command line
+bind '"\ep": " \C-e\C-u`__fzf_history__`\e\C-e\e^\er"'
 
-    # Alt-j cd into the selected directory
-    bind '"\ej": " \C-e\C-u`__fzf_cd__`\e\C-e\er\C-m"'
+# Alt-j cd into the selected directory
+bind '"\ej": " \C-e\C-u`__fzf_cd__`\e\C-e\er\C-m"'
 
-    # Alt-i - Paste the selected file path into the command line
-    if [ $BASH_VERSINFO -gt 3 ]; then
-        bind -x '"\ei": "fzf-file-widget"'
-    elif __fzf_use_tmux__; then
-        bind '"\ei": " \C-u \C-a\C-k`__fzf_select_tmux__`\e\C-e\C-y\C-a\C-d\C-y\ey\C-h"'
-    else
-        bind '"\ei": " \C-u \C-a\C-k`__fzf_select__`\e\C-e\C-y\C-a\C-y\ey\C-h\C-e\er \C-h"'
-    fi
+# Alt-i - Paste the selected file path into the command line
+if [ $BASH_VERSINFO -gt 3 ]; then
+    bind -x '"\ei": "fzf-file-widget"'
+elif __fzf_use_tmux__; then
+    bind '"\ei": " \C-u \C-a\C-k`__fzf_select_tmux__`\e\C-e\C-y\C-a\C-d\C-y\ey\C-h"'
+else
+    bind '"\ei": " \C-u \C-a\C-k`__fzf_select__`\e\C-e\C-y\C-a\C-y\ey\C-h\C-e\er \C-h"'
 fi
