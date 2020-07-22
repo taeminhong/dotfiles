34107fb81698323cc39b33834be037a4f538002b
Taemin Hong <taeminhong@outlook.com>
2020-07-22 21:43:22 +0900

    Remove copy-pasted z.sh and sensible-defaults.el
    z.sh and sensible-defaults.el will be downloaded on the fly if they needed.

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,6 +9,16 @@ fi
 cd "$(dirname "${source}")" || exit 1
 unset source
 
+# directory in which files are saved temporarily.
+# this will be deleted on exit.
+downloads=$(mktemp -d)
+
+cleanup () {
+    rm -rf "$downloads"
+}
+
+trap cleanup EXIT
+
 # rsync is more suitable in this task, but it's not available on Git for Windows
 cp -a \
    .bash_logout \
@@ -27,11 +37,32 @@ cp -a \
    .minttyrc \
    .inputrc \
    .ghci \
-   z.sh \
    tmux.sh \
    ~
-mkdir -p ~/.emacs.d && cp -a .emacs.d/local ~/.emacs.d
-mkdir -p ~/.ssh && cp -a .ssh/config ~/.ssh
+mkdir -p ~/.emacs.d/local
+cp -a .emacs.d/local/taemin.el ~/.emacs.d/local
+
+mkdir -p ~/.ssh
+cp -a .ssh/config ~/.ssh
+
+wget_unzip () {
+    wget -qP"$2" "$1" && unzip -qd"$2" "$2/$(basename "$1")"
+}
+
+# z - directory jump utility
+if test ! -e ~/z.sh
+then
+    wget_unzip https://github.com/rupa/z/archive/v1.11.zip "$downloads" && \
+        cp "$downloads/z-1.11/z.sh" ~
+fi
+
+# sensible-defaults - sensible emacs settings
+if test ! -e ~/.emacs.d/local/sensible-defaults.el
+then
+    wget_unzip https://github.com/hrs/sensible-defaults.el/archive/main.zip "$downloads" && \
+        cp "$downloads/sensible-defaults.el-main/sensible-defaults.el" ~/.emacs.d/local && \
+        echo "(provide 'sensible-defaults)" >>~/.emacs.d/local/sensible-defaults.el
+fi
 
 if command -v tmux >/dev/null 2>&1
 then
