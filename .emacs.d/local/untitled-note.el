6860ff79209065f75dccc8307578f8f92066f788
Taemin Hong <taeminhong@outlook.com>
2020-08-24 13:39:55 +0900

    untitled-note.el: remove the temporary file after use
    After killing the buffer or writting contents into the other file, the temporary
    file is not needed anymore so delete it.

--- .emacs.d/local/untitled-note.el
+++ .emacs.d/local/untitled-note.el
@@ -2,6 +2,12 @@
   (file-name-as-directory
    (expand-file-name "untitled-note" user-emacs-directory)))
 
+(defvar untitled-note-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "C-x C-w") #'untitled-note-write-file)
+    map)
+  "Keymap for `untitled-note-mode'")
+
 (define-minor-mode untitled-note-mode
   "Untitled note mode."
   nil
@@ -9,6 +15,14 @@
   '()
   (when untitled-note-mode
     (cd (getenv "PWD"))))
+
+(defun untitled-note--cleanup ()
+  (when (and untitled-note-mode buffer-file-name)
+    (untitled-note--delete-file-and-backup buffer-file-name)))
+
+(defun untitled-note--delete-file-and-backup (file-name)
+  (delete-file file-name)
+  (delete-file (make-backup-file-name file-name)))
 
 (defun untitled-note-new-note ()
   "Create a new note"
@@ -34,12 +48,18 @@
    t
    (lambda () (and buffer-file-name untitled-note-mode))))
 
-;; exclude untitled note files from the recentf list
+(defun untitled-note-write-file ()
+  "Write contents into the other file and remove the temporary file"
+  (interactive)
+  (let ((old-file-name buffer-file-name))
+    (call-interactively 'write-file)
+    (unless (string= buffer-file-name old-file-name)
+      (untitled-note--delete-file-and-backup old-file-name))))
+
 (eval-after-load 'recentf
   '(add-to-list 'recentf-exclude untitled-note-directory))
-
 (make-directory untitled-note-directory t)
-
 (advice-add 'save-buffers-kill-terminal :before #'untitled-note-save-all)
+(add-hook 'kill-buffer-hook #'untitled-note--cleanup)
 
 (provide 'untitled-note)
