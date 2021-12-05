taeminhong/dotfiles

commit b0b074c1da4f9dab079f10364c508037f9dff060

.profile: add /opt/local to include and library path
addpath() has been rewritten so that you can apply it to other path variables.

While we're at it, changed the definition of INITIAL_PATH because INITIAL_PATH
can be reassigned when it has been declared but is set to null.

.profile
@@ -3,30 +3,46 @@
 # Git coding guidelines will help you write portable shell scripts
 # https://github.com/git/git/blob/master/Documentation/CodingGuidelines
 
-export LANG=en_US.UTF-8
-export LC_ALL="$LANG"
-export INITIAL_PATH="${INITIAL_PATH:-$PATH}"
+: ${INITIAL_PATH=$PATH}
+: ${INITIAL_C_INCLUDE_PATH=$C_INCLUDE_PATH}
+: ${INITIAL_CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH}
+: ${INITIAL_LIBRARY_PATH=$LIBRARY_PATH}
 
+PATH="$INITIAL_PATH"
+C_INCLUDE_PATH="$INITIAL_C_INCLUDE_PATH"
+CPLUS_INCLUDE_PATH="$INITIAL_CPLUS_INCLUDE_PATH"
+LIBRARY_PATH="$INITIAL_LIBRARY_PATH"
+
+# Prepend the given path to the variable.
+# usage: addpath VAR PATH
 addpath () {
-    test -d "$1" && PATH="$1:$PATH"
+    eval "test -d \"$2\" && $1=\"$2\${$1:+:\$$1}\""
 }
 
-# Rebuild PATH
-PATH="$INITIAL_PATH"
 # MacPort
-addpath /opt/local/bin
-addpath /opt/local/sbin
-addpath "/Applications/Racket v7.5/bin"
+addpath PATH /opt/local/bin
+addpath PATH /opt/local/sbin
+addpath PATH "/Applications/Racket v7.5/bin"
 # allow running commands installed by `pip install --user` on the Mac
-addpath "$HOME/Library/Python/3.6/bin"
+addpath PATH "$HOME/Library/Python/3.6/bin"
 # Cargo, a package manager for Rust
-addpath "$HOME/.cargo/bin"
+addpath PATH "$HOME/.cargo/bin"
 # Yarn, a package manager for JS
-addpath "$HOME/.yarn/bin"
+addpath PATH "$HOME/.yarn/bin"
 # Haskell
-addpath "$HOME/.ghcup/bin"
-addpath "$HOME/.cabal/bin"
+addpath PATH "$HOME/.ghcup/bin"
+addpath PATH "$HOME/.cabal/bin"
 # systemd user bin directory
-addpath "$HOME/.local/bin"
+addpath PATH "$HOME/.local/bin"
+
+addpath C_INCLUDE_PATH     /opt/local/include
+addpath CPLUS_INCLUDE_PATH /opt/local/include
+addpath LIBRARY_PATH       /opt/local/lib
 
 unset addpath
+
+export LANG=en_US.UTF-8
+export LC_ALL="$LANG"
+export INITIAL_C_INCLUDE_PATH C_INCLUDE_PATH
+export INITIAL_CPLUS_INCLUDE_PATH CPLUS_INCLUDE_PATH
+export INITIAL_LIBRARY_PATH LIBRARY_PATH

commit 66fa1778a2df127720dc0927a02a0f07fa579551

.profile: avoid duplicate in PATH variable
Commit b0b074c1d(".profile: add /opt/local to include and library path," 2020-10-19)
accidentally dropped the INITIAL_PATH export from .profile, and this caused
duplicates in the PATH variable when using Tmux.

This patch restores the INITIAL_PATH export.

.profile
@@ -40,6 +40,7 @@ unset addpath
 
 export LANG=en_US.UTF-8
 export LC_ALL="$LANG"
+export INITIAL_PATH
 export INITIAL_C_INCLUDE_PATH C_INCLUDE_PATH
 export INITIAL_CPLUS_INCLUDE_PATH CPLUS_INCLUDE_PATH
 export INITIAL_LIBRARY_PATH LIBRARY_PATH