a47a2774af371f8d5a5f75318cf7e04d17d31c4b
Taemin Hong <taeminhong@outlook.com>
2022-11-11 12:33:42 +0900

    fzf: set up keybindings and auto completion for MacPorts fzf
    The keybindings and auto completion are automatically set up if you build
    and install fzf manually from the source. But that's not the case if you
    install fzf via a package manager like Homebrew or MacPorts.
    
    This patch teaches Mac/setup to setup the keybindings and auto-completion
    even if you install fzf via a package manager.

---
+++ Mac/fzf-macports.bash
@@ -0,0 +1,7 @@
+# Auto-completion
+# ---------------
+source "/opt/local/share/fzf/shell/key-bindings.bash" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/opt/local/share/fzf/shell/completion.bash"
---
+++ Mac/fzf-macports.zsh
@@ -0,0 +1,7 @@
+# Auto-completion
+# ---------------
+source "/opt/local/share/fzf/shell/key-bindings.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/opt/local/share/fzf/shell/completion.zsh"
