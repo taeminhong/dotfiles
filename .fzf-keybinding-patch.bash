1ab8119366ce30917021b930c41b23ca2ece02cd
Taemin Hong <taeminhong@outlook.com>
2022-11-11 15:30:22 +0900

    fzf: remap for all three keymaps
    As mcornella pointed out, before v0.30.0, fzf only has set up the current
    keymap. https://github.com/junegunn/fzf/issues/2694
    
    fzf v0.30.0 changed to set up bindings for all three keymaps
    (emacs, vicmd, and viins). But the keybinding patch still changes the current
    keymap only. Let's make the patch update all keymaps.
    
    While at it, we can improve the keybinding patch using the method guengunn
    suggested. It makes the keybinding patch independent from the fzf implementation
    details. See https://github.com/junegunn/fzf/issues/2694

--- .fzf-keybinding-patch.bash
+++ .fzf-keybinding-patch.bash
@@ -1,19 +1,58 @@
-# roll back keybindings
-bind '"\C-t": transpose-chars'
-bind '"\ec": capitalize-word'
-bind '"\C-r": reverse-search-history'
+# See junegunn's suggestion for remapping
+# https://github.com/junegunn/fzf/issues/546#issuecomment-213289351
 
-# Alt-p Paste the selected command from history into the command line
-bind '"\ep": " \C-e\C-u`__fzf_history__`\e\C-e\e^\er"'
+# Default readline key bindings
+# https://www.man7.org/linux/man-pages/man3/readline.3.html#DEFAULT_KEY_BINDINGS
 
-# Alt-j cd into the selected directory
-bind '"\ej": " \C-e\C-u`__fzf_cd__`\e\C-e\er\C-m"'
+# Bash 5.1.16 seems to have a bug that overwritting a shell command binding
+# breaks the keymap. To workaround, we need to delete the binding first.
+#
+# Example:
+#   $ bind -x '"\C-h": echo "hello"'
+#   $ bind -x '"\C-w": echo "world"'
+#   $ bind '"\C-w": transpose-chars' # <- overwrite
+#   $ bind -X
+#   "\C-w": "echo \"world\""
+#   bash: bash_execute_unix_command: cannot find keymap for command # <- when pressing C-h
+#
+# Workaround:
+#   $ bind -x '"\C-h": echo "hello"'
+#   $ bind -x '"\C-w": echo "world"'
+#   $ bind -r "\C-w" # <- delete it first
+#   $ bind '"\C-w": transpose-chars'
+#   $ bind -X
+#   "\C-h": "echo \"hello\""
 
-# Alt-i - Paste the selected file path into the command line
-if [ $BASH_VERSINFO -gt 3 ]; then
-    bind -x '"\ei": "fzf-file-widget"'
-elif __fzf_use_tmux__; then
-    bind '"\ei": " \C-u \C-a\C-k`__fzf_select_tmux__`\e\C-e\C-y\C-a\C-d\C-y\ey\C-h"'
-else
-    bind '"\ei": " \C-u \C-a\C-k`__fzf_select__`\e\C-e\C-y\C-a\C-y\ey\C-h\C-e\er \C-h"'
-fi
+dup_bind () {
+    # escape backslashes
+    local from="${2//\\/\\\\}"
+    local to="${3//\\/\\\\}"
+    local match1="$(bind -m "$1" -s | grep "^\"$from")"
+    [ -n "$match1" ] && bind -m "$1" "$(echo "$match1" | sed "s/$from/$to/g")"
+    local match2="$( (( BASH_VERSINFO[0] > 3 )) && bind -m "$1" -X | grep "^\"$from")"
+    [ -n "$match2" ] && bind -m "$1" -x "$(echo "$match2" | sed "s/$from/$to/")"
+}
+
+# Ctrl-R -> Alt-P
+# Paste the selected command from history into the command line
+for keymap in emacs vi vi-insert; do
+    dup_bind "$keymap" "\C-r" "\ep"
+    bind -m "$keymap" -r "\C-r"
+    bind -m "$keymap" '"\C-r": reverse-search-history'
+done
+
+# Alt-C -> Alt-J
+# cd into the selected directory
+for keymap in emacs vi vi-insert; do
+    dup_bind "$keymap" "\ec" "\ej"
+    bind -m "$keymap" -r "\ec"
+done
+bind -m emacs '"\ec": capitalize-word'
+
+# Ctrl-T -> Alt-I
+# Paste the selected file path into the command line
+for keymap in emacs vi vi-insert; do
+    dup_bind "$keymap" "\C-t" "\ei"
+    bind -m "$keymap" -r "\C-t"
+    bind -m "$keymap" '"\C-t": transpose-chars'
+done
