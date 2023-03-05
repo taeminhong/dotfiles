9e4a509263e3606e1f4bda9d1e15826510d82258
Taemin Hong <taeminhong@outlook.com>
2023-03-05 16:14:08 +0900

    Emacs: tweak ansi-colors for readibility
    There is not enough contrast between black and ansi-color-blue. So blue
    texts in an embedded terminal are quite unreadable. The same thing goes
    for ansi-color-red, green, and yellow.
    
    Let's brighten up ansi-colors to improve readability.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -383,7 +383,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(ansi-color-blue ((t (:background "dodgerblue" :foreground "dodgerblue"))))
+ '(ansi-color-bright-blue ((t (:background "deepskyblue" :foreground "deepskyblue"))))
+ '(ansi-color-green ((t (:background "lawngreen" :foreground "lawngreen"))))
+ '(ansi-color-bright-green ((t (:background "greenyellow" :foreground "greenyellow"))))
+ '(ansi-color-yellow ((t (:background "orange" :foreground "orange"))))
+ '(ansi-color-bright-yellow ((t (:background "gold" :foreground "gold"))))
+ '(ansi-color-red ((t (:background "deeppink" :foreground "deeppink"))))
+ '(ansi-color-bright-red ((t (:background "hotpink" :foreground "hotpink")))))
 (put 'narrow-to-page 'disabled nil)
 (put 'narrow-to-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
