taeminhong/dotfiles

commit aed5242a81d42e0111d5e0c72bd611091fb02c28

Emacs: bind isearch-yank-char to C-c
I couldn't have recalled that isearch-yank-char is bound to C-M-y.
Give it a more memorable key binding, C-c.

.emacs.d/init.el
@@ -238,6 +238,10 @@
               ("M-o")
               ("M-g")))
 
+(use-package isearch
+  :bind (:map isearch-mode-map
+              ("C-c" . isearch-yank-char)))
+
 (use-package diff-mode
   :bind (:map diff-mode-map
               ("M-o")))

commit 1a17deed519ecda294fdab8b3dc9c68b01b160cd

Emacs: Fix key sequence error in eshell
The commit aed5242a81 ("Emacs: bind isearch-yan-char to C-c", 2021-01-16)
introduced a new keybinding for isearch mode, but as a consequence of this
change, eshell fails to initialize and gives the error message "error: Key
sequence C-c C-c starts with non-prefix key C-c."

Actually, I barely use C-c key in isearch mode. Let's drop the keybinding to
restore eshell's functionality.

.emacs.d/init.el
@@ -248,10 +248,6 @@
               ("M-o")
               ("M-g")))
 
-(use-package isearch
-  :bind (:map isearch-mode-map
-              ("C-c" . isearch-yank-char)))
-
 (use-package diff-mode
   :bind (:map diff-mode-map
               ("M-o")))