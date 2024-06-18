d72fd75a429fba9dc192963b5b2ed65b1eaa3c6c
Taemin Hong <taeminhong@outlook.com>
2024-06-18 23:38:56 +0900

    Emacs: Mark apheleia variable as safe
    This commit registers apheleia-inhibit to safe-local-variable-values list to
    suppress prompts asking whether to apply it or not.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -421,7 +421,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(hydra typescript-mode yaml-mode nginx-mode diminish dante ivy-xref dumb-jump w3m undo-fu goto-last-change expand-region markdown-mode haskell-mode anaconda-mode which-key cider adjust-parens avy flx rainbow-delimiters geiser paredit company pcre2el glsl-mode magit smex use-package json-mode js2-mode csharp-mode counsel ag vue-mode)))
+   '(hydra typescript-mode yaml-mode nginx-mode diminish dante ivy-xref dumb-jump w3m undo-fu goto-last-change expand-region markdown-mode haskell-mode anaconda-mode which-key cider adjust-parens avy flx rainbow-delimiters geiser paredit company pcre2el glsl-mode magit smex use-package json-mode js2-mode csharp-mode counsel ag vue-mode))
+ '(safe-local-variable-values '((apheleia-inhibit . t))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
