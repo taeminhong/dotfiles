3acf883bef7c05c2b5caf75d2aaf34c1714c8b0f
Taemin Hong <taeminhong@outlook.com>
2025-10-27 15:30:00 +0900

    Emacs: Keybinding for w3m search

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -429,7 +429,8 @@
   :init
   (setq w3m-home-page "https://duckduckgo.com/lite")
   (setq w3m-search-default-engine "duckduckgo")
-  :bind ((:map w3m-mode-map
+  :bind (("M-s w" . w3m-search)
+         (:map w3m-mode-map
                ("C-e" . move-end-of-line)
                ("C-a" . move-beginning-of-line)
                ("<" . w3m-tab-previous-buffer)
